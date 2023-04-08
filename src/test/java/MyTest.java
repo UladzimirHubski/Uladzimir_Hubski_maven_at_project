14702def17272c4c612537541700e5efe942b07b
Uladzimir Hubski <vladimirgubskij@gmail.com>
2023-04-08 12:58:48 +0300

    Create Runners

--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -3,8 +3,6 @@ import org.junit.Test;
 
 public class MyTest {
     Calculator calc = new Calculator();
-    EvenOddChecker even = new EvenOddChecker();
-    Rectangle rec = new Rectangle(4.0, 6.0);
 
 
     //Calculator
@@ -35,49 +33,5 @@ public class MyTest {
     @Test(expected = ArithmeticException.class)
     public void MyDivideZeroTest() {
         Assert.assertEquals("Addition returned wrong result: ", ArithmeticException.class, calc.divide(2, 0));
-    }
-    //EvenOddChecker
-
-    @Test
-
-    public void MyLineTrueTest() throws Exception {
-        Assert.assertTrue(even.check(6));
-    }
-
-    @Test
-    public void MyLineFalseTest() throws Exception {
-        Assert.assertFalse(even.check(5));
-    }
-
-
-//Rectangle
-
-    @Test
-    public void getLTest() {
-        Assert.assertEquals(4.0, rec.getLength(), 0.0);
-    }
-
-    @Test
-    public void setLTest() {
-        rec.setLength(5.0);
-        Assert.assertEquals(5.0, rec.getLength(), 0.0);
-    }
-
-    @Test
-    public void getWTest() {
-
-        Assert.assertEquals(6.0, rec.getWidth(), 0.0);
-    }
-
-    @Test
-    public void setWTest() {
-        rec.setWidth(5.0);
-        Assert.assertEquals(5.0, rec.getWidth(), 0.0);
-    }
-
-    @Test
-    public void getAreaTest() {
-
-        Assert.assertEquals(24.0, rec.getArea(), 0.0);
     }
 }
