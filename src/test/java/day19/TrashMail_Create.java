83e9cc31022da71a033f31f7da6abfe10efed60c
Uladzimir Hubski <vladimirgubskij@gmail.com>
2023-05-06 08:33:44 +0300

    Create steps

--- src/test/java/day19/TrashMail_Create.java
+++ src/test/java/day19/TrashMail_Create.java
@@ -2,15 +2,12 @@ package day19;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 
-import javax.swing.*;
-import javax.xml.xpath.XPath;
 import java.time.Duration;
 
-public class CreateEmail {
+public class TrashMail_Create {
     public static void main(String[] args) {
         WebDriver driver = new ChromeDriver();
         Actions make = new Actions(driver);
@@ -19,13 +16,13 @@ public class CreateEmail {
         driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(5));
         driver.get("https://trashmail.com/");
 
-        make.doubleClick(driver.findElement(By.xpath("//input[@id='fe-name']"))).sendKeys("19vova93").perform();
+        make.doubleClick(driver.findElement(By.xpath("//input[@id='fe-name']"))).sendKeys("--------").perform();
 
         driver.findElement(By.xpath("//input[@id='fe-forward']")).sendKeys("19vova93@mail.ru");
         driver.findElement(By.xpath("//div[@class='input-group input-group-sm input-group-btn']//div[@class='input-group-btn']")).click();
         driver.findElement(By.xpath("//div[@class='input-group input-group-sm input-group-btn']//div[@class='input-group-btn']//li[@role='presentation'][3]")).click();
         driver.findElement(By.xpath("//div[1][@class='input-group input-group-sm input-group-btn']//div[@class='input-group-btn']")).click();
         driver.findElement(By.xpath("//div[1][@class='input-group input-group-sm input-group-btn']//div[@class='input-group-btn']//li[@role='presentation'][1]")).click();
-        //driver.findElement(By.xpath("//button[@id=\"fe-submit\"]")).click();
+        driver.findElement(By.xpath("//button[@id=\"fe-submit\"]")).click();
     }
 }
